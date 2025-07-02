612a5565766aa2845470859671b057030a926db6
Abhinav-36 <vabhinav991222@gmail.com>
2025-07-02 19:39:11 +0530

    code changes

---
+++ src/Displayname.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";   
+
+export default  function Displayname(){
+    const [fullName,setFullName] = useState({
+        firstname: "",
+        lastname: "",
+        isNameSet: false 
+    })
+     return (
+        <div style={{padding: "10px", fontFamily: "Times New Roman, Times, serif"}}>
+            <form onSubmit={(e)=> { 
+                e.preventDefault();
+                setFullName({...fullName,isNameSet: true})
+                 }}>
+            <h1>Full Name Display</h1>
+            <p style={{display: "inline"}}>First Name:</p> 
+            <input type="text" required value = {fullName.firstname} onChange={(e) => {setFullName({...fullName,firstname: e.target.value})}} /> <br/>
+            <p style={{display: "inline"}}  >Last Name:</p> 
+            <input type="text" required value={fullName.lastname}  onChange={(e) => {setFullName({...fullName,lastname: e.target.value})}}/><br/>
+            <button type="submit" >Submit</button>
+        </form>
+            {
+                fullName.isNameSet ? <p>Full Name: {`${fullName.firstname} ${fullName.lastname}`}</p> : <p></p>
+            }
+        </div>
+        
+    );
+}
